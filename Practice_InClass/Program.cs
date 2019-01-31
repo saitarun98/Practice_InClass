45c3b80fac42bc1119f7d119e3bf1fde964a822d
Saitarun Irigala <saitarun@mail.usf.edu>
2019-01-31 17:59:26 -0500

    v1.0

--- Practice_InClass/Program.cs
+++ Practice_InClass/Program.cs
@@ -6,7 +6,23 @@ namespace Practice_InClass
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter your name: ");
+
+            try
+            {
+                string input = Console.ReadLine();
+                Console.WriteLine("Hello " + input + ".");
+                Console.WriteLine("Press any key to exit.");
+                Console.ReadKey(true);
+            }//end of try
+
+            catch
+            {
+                Console.WriteLine("Please enter a valid name!");
+                Console.WriteLine("Pelase exit and try again.");
+                Console.ReadKey(true);
+            }//end of catch
+                ;
         }
     }
 }
